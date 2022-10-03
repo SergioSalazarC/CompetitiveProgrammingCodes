32def0b817118baa81b8ac301600006e5410ef5e
SergioSalazarC <serxuki2000@gmail.com>
2022-10-03 12:43:23 +0200

    Nuevos Problemas

---
+++ problemsII/gen.java
@@ -0,0 +1,9 @@
+public class gen {
+    public static void main(String[] args) {
+        System.out.println(100000);
+        for (int i = 0; i < 100000/2; i++) {
+            System.out.println("0 0 1 1");
+            System.out.println("1 1 0 0");
+        }
+    }
+}
